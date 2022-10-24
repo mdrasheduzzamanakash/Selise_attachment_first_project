d19f71f7a08629fa19a6754b9ab85d99a9750b10
Md. Rasheduzzaman <mdrasheduzzamanakash@gmail.com>
2022-10-24 18:34:30 +0600

    add interactive input taking and displaying

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello')
+var input = document.getElementById("inputField");
+var display = document.getElementById("displayField");
+var button = document.getElementById("buttonField");
+
+
+button.addEventListener('click', clickHandler);
+
+
+function clickHandler() {
+    var inputValue = input.value;
+    display.innerText = inputValue.trim();
+}
